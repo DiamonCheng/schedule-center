014fc552447651719162f5d9da48a6bd2062fc77
Defferson.Cheng <dcphone33@live.com>
2017-02-10 10:12:38 +0800

    清空目录
    Signed-off-by: Defferson.Cheng <dcphone33@live.com>

--- frame/src/main/java/com/frame/dao/GeneralDao.java
+++
@@ -1,8 +0,0 @@
-package com.frame.dao;
-
-import org.springframework.stereotype.Repository;
-
-import com.frame.core.components.DaoSupport;
-
-@Repository
-public class GeneralDao extends DaoSupport{}
