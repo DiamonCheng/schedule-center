407b59bc130dea7e4db2172581068f1b1ccdbc76
DeffersonCheng <dcphone33@live.com>
2017-11-08 22:03:36 +0800

    做了默认用户密码可配置化； 对消费消息之后的成功与失败场景逻辑操作进行编码。
    --TODO 对失败的任务进行异常管理;对任务调度进行测试（包括成功场景，失败场景，多实例场景）
    
    Signed-off-by: DeffersonCheng <dcphone33@live.com>

--- schedule-core/src/main/java/com/frame/core/webapp/controller/account/UserController.java
+++ schedule-core/src/main/java/com/frame/core/webapp/controller/account/UserController.java
@@ -4,6 +4,7 @@ import com.frame.core.entity.UserEntity;
 import com.frame.core.query.xml.GeneralController;
 import com.frame.core.query.xml.annoation.PageDefinition;
 import com.frame.core.utils.EncriptUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @PageDefinition
 public class UserController extends GeneralController<UserEntity> {
-	private final static String DEFAULT_PASSWORD="233333";
+	@Value("${manage.user.default.password}")
+	private String DEFAULT_PASSWORD="233333";
 	@Override
 	public boolean beforeUpdate(UserEntity entity) {
 		if (entity.getId()==null){
--- schedule-service/src/main/java/com/frame/schedule/service/impl/TaskRecordService.java
+++ schedule-service/src/main/java/com/frame/schedule/service/impl/TaskRecordService.java
@@ -1,9 +1,14 @@
 package com.frame.schedule.service.impl;
 
+import com.frame.core.dao.GeneralDao;
+import com.frame.schedule.entity.TaskRecordEntity;
 import com.frame.schedule.sdk.structure.ScheduleTaskMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * <p>Descriptions...
@@ -12,7 +17,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class TaskRecordService {
     private Logger logger= LoggerFactory.getLogger(this.getClass());
-    public void taskFinish(ScheduleTaskMessage scheduleTaskMessage){
     
+    @Autowired
+    private GeneralDao generalDao;
+    
+    public void taskFinish(ScheduleTaskMessage scheduleTaskMessage){
+        logger.info("A task receipt accepted."+scheduleTaskMessage);
+        TaskRecordEntity taskRecordEntity=generalDao.getHibernateTemplate().get(TaskRecordEntity.class,scheduleTaskMessage.getTaskRecordId());
+        assert taskRecordEntity!=null:"Con not find task record entity with id:"+scheduleTaskMessage.getTaskRecordId();
+        taskRecordEntity.setFinishDateTime(new Date());
+        if (scheduleTaskMessage.isSuccess()){
+            taskRecordEntity.setStatus(TaskRecordEntity.Status.SUCCESS.toString());
+        }else{
+            taskRecordEntity.setStatus(TaskRecordEntity.Status.WAIT_RETRY.toString());
+            //TODO process Error content
+            logger.error("A task process failed with record: "+taskRecordEntity+"Error content:"+scheduleTaskMessage.getErrorContent());
+        }
+        generalDao.getHibernateTemplate().update(taskRecordEntity);
     }
 }
